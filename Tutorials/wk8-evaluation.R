ad815251a363168d4c1a7f5d70e964596cdc348b
Yuting Ma <michelle_ma927@hotmail.com>
2016-03-11 11:49:03 -0500

    update evaluation code

--- Tutorials/wk8-evaluation.R
+++ Tutorials/wk8-evaluation.R
@@ -11,8 +11,10 @@ ind_cat <- which(eval_label == 1) # 1000 cats
 ind_dog <- which(eval_label == 0) # 1000 dogs
 n_cat_fold <- n_dog_fold <- 200
 
-CV_err <- rep(0, n_rep)
-CV_fit <- array(dim=c(n, n_rep))
+CV_err_baseline <- rep(0, n_rep)
+CV_err_adv <- rep(0, n_rep)
+CV_fit_baseline <- array(dim=c(n, n_rep))
+CV_fit_adv <- array(dim=c(n, n_rep))
 train_time <- array(dim=c(K, n_rep))
 
 for(r in 1:n_rep){
@@ -34,13 +36,19 @@ for(r in 1:n_rep){
     label_test <- label_eval[ind_test]
     train_time[c,r] <- system.time(mod_train <- train(dat_train, label_train))[1]
     pred_test <- test(mod_train, dat_test)
-    CV_fit[ind_test, r] <- pred_test
+    CV_fit_baseline[ind_test, r] <- pred_test$baseline
+    CV_fit_adv[ind_test, r] <- pred_test$adv
   }
-  cv_err[r] <- mean(CV_fit[,r] != label_eval)
+  cv_err_baseline[r] <- mean(CV_fit_baseline[,r] != label_eval)
+  cv_err_adv[r] <- mean(CV_fit_adv[,r] != label_eval)
+  
 }
 
-save(CV_fit, cv_err, train_time, file="CV_result.RData")
+save(CV_fit_baseline, CV_fit_adv,  cv_err_baseline, cv_err_adv, train_time, file="CV_result.RData")
 
-cat("Mean of CV Error =", round(mean(CV_err), digits=4), "\n")
-cat("SD of CV Error =", round(sd(CV_err), digits=4), "\n")
+cat("Mean of Baseline CV Error =", round(mean(CV_err_baseline), digits=4), "\n")
+cat("SD of Baseline CV Error =", round(sd(CV_err_baseline), digits=4), "\n")
+cat("Mean of Adv CV Error =", round(mean(CV_err_adv), digits=4), "\n")
+cat("SD of Adv CV Error =", round(sd(CV_err_adv), digits=4), "\n")
+
 cat("Mean Training Name =", round(mean(train_time), digits=2), "\n")
